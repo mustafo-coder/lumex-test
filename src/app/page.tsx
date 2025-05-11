b5ae570242dd2e4947365a89e2661634aa757991
mustafo-coder <shaeduloffdev@gmail.com>
2025-05-11 17:04:36 +0500

    optimize & finish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { Cube } from "@/components";
 import dynamic from "next/dynamic";
 
 const About = dynamic(() => import("@/components/About"));
 const Hero = dynamic(() => import("@/components/Hero"));
+const Cube = dynamic(() => import("@/components/Cube"));
 
 const page = () => {
   return (
--- src/components/Cube.tsx
+++ src/components/Cube.tsx
@@ -1,10 +1,17 @@
+"use client";
 import Spline from "@splinetool/react-spline";
+import { useState } from "react";
 
 const Cube = () => {
+  const [loaded, setLoaded] = useState(false);
+
   return (
     <Spline
-      className="fixed max-lg:scale-90 max-md:scale-75 top-1/2 z-50 transition-all left-1/2 -translate-x-[52%] -translate-y-[44%] pointer-events-none select-none"
+      className={`fixed max-lg:scale-90 max-md:scale-75 top-1/2 z-40 transition-opacity duration-700 left-1/2 -translate-x-[52%] -translate-y-[44%] pointer-events-none select-none ${
+        loaded ? "opacity-100" : "opacity-0"
+      }`}
       scene="https://prod.spline.design/GQvjbz5XcjTmzV-H/scene.splinecode"
+      onLoad={() => setLoaded(true)}
     />
   );
 };
