c45984331ed0bcdb7466f7ea829981744b0104eb
mustafo-coder <shaeduloffdev@gmail.com>
2025-05-12 13:48:49 +0500

    fix camera

--- src/components/Cube.tsx
+++ src/components/Cube.tsx
@@ -10,7 +10,7 @@ const Cube = () => {
       className={`fixed max-lg:scale-90 max-md:scale-75 top-1/2 z-40 transition-opacity duration-700 left-1/2 -translate-x-[52%] -translate-y-[44%] pointer-events-none select-none ${
         loaded ? "opacity-100" : "opacity-0"
       }`}
-      scene="https://prod.spline.design/GQvjbz5XcjTmzV-H/scene.splinecode"
+      scene="https://prod.spline.design/fUHEUhHuYtHbRJEK/scene.splinecode"
       onLoad={() => setLoaded(true)}
     />
   );
